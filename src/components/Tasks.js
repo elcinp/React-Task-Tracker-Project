3a882deb807a38a27425966d8494c829fb2caef0
elcinp <elcinpars1@gmail.com>
2021-09-18 23:29:24 +0300

    Tasks mapped,onDelete function (props)

---
+++ src/components/Tasks.js
@@ -0,0 +1,15 @@
+import React,{useState} from 'react'
+// import {initialState} from '../store/initialStates'
+import Task from './Task'
+
+export default function Tasks({tasks,onDelete}) {
+    
+    return (
+        <div>
+            {tasks.map((task )=>(
+                <Task key={task.id} task={task} onDelete={onDelete}/>
+            ))}
+        </div>
+    )
+}
+
