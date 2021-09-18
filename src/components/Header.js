c85a2b4892114ebfee6824ffe27554cc0b3c6366
elcinp <elcinpars1@gmail.com>
2021-09-18 23:27:31 +0300

    Header and Button components with props

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({title}) => {
+    return (
+        <header className="header">
+            <h1>{title}</h1>
+            <Button color="purple" text="Show create a task button"/>
+        </header>
+    )
+}
+// Header.defaultProps = {
+//     title:'Task Tracker as default'
+// }
+
+
+// Header.PropTypes = {
+//     title: PropTypes.string.isRequired
+// }
+
+
+
+export default Header
