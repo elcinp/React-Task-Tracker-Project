c1ca9a3a82b1719b6dc29c5247657830c74bfaa7
elcinp <elcinpars1@gmail.com>
2021-09-18 23:55:42 +0300

    onClick event added(onDelete)

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,12 +1,14 @@
 import React from "react"
 import { FaTimes } from 'react-icons/fa'
 
-const Task = ({task}) => {
+const Task = ({task,onDelete}) => {
     return (
         <div className={"task"}>
             <h3>
                 {task.text}
-                <FaTimes/>
+                <FaTimes onClick={()=> {
+                    onDelete(task.id)
+                }}/>
             </h3>
             <p>{task.day}</p>
         </div>
