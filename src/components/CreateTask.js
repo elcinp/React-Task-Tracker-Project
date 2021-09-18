e1bc223410ce13ebc47d29eefc52bf282aa1e60e
elcinp <elcinpars1@gmail.com>
2021-09-18 23:59:32 +0300

    asd

--- src/components/CreateTask.js
+++ src/components/CreateTask.js
@@ -2,6 +2,7 @@ import React,{useState} from 'react'
 
 const CreateTask = () => {
     const [text,setText] = useState("")
+    const [dat,setText] = useState("")
     return (
         <div>
             
